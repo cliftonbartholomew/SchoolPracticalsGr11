a7965f79fda291481ae33be499010ea7968917a1
Cliftonb <Cliftonb@ZAKZRHB-CL21.ballito.local>
2021-02-09 12:52:28 +0200

    Grade 11 Term 1 2021 - Practice Practical Code

---
+++ SchoolPracticals/src/main/java/PracticePrac/VowelsOnly.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PracticePrac;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Cliftonb
+ */
+public class VowelsOnly {
+    public static void main(String[] args) {
+        String input = JOptionPane.showInputDialog("Enter in a string");
+        String output = "";
+        
+        //Check if a char is in this string
+        String vowels = "aeiou";
+        
+        //Cycle through each letter
+        for(int i = 0; i < input.length(); i ++){
+            
+            //if a letter is in the vowels string - keep it
+            //Note: contains takes in a string. "" + char is a quick conversion to a string (this works for all primitive types).
+            if(vowels.contains("" + input.charAt(i))){
+                output += input.charAt(i);
+            }
+        }
+        
+        System.out.println(output);
+    }
+}
