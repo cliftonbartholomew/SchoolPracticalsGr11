e60852b31c1022ac3cbef6ef80d29d3b8ace313c
Cliftonb <Cliftonb@ZAKZRHB-CL21.ballito.local>
2022-01-22 12:50:29 +0200

    Intro to the character class

---
+++ src/main/java/Strings/CharsIntro.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Strings;
+
+/**
+ *
+ * @author Cliftonb
+ */
+public class CharsIntro {
+
+	public static boolean isDigit(char in) {
+		switch (in) {
+			case '0':
+			case '1':
+			case '2':
+				return true;
+			default:
+				return false;
+		}
+	}
+
+	private static boolean howsit() {
+		return false;
+	}
+
+	public static void main(String[] args) {
+	}
+}
---
+++ src/main/java/Strings/CharsIntroUI.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Strings;
+
+/**
+ *
+ * @author Cliftonb
+ */
+public class CharsIntroUI {
+
+	public static void main(String[] args) {
+		char c = 'c';
+		System.out.println(CharsIntro.isDigit(c));
+
+		String str1 = "howsit";
+		str1.equals("sfafd");
+
+		String str2 = "adflaksjdf";
+		String str3 = "adfaf";
+		String str4 = "sdfasf";
+
+		System.out.println(str1.equals(str2));
+
+	}
+}
